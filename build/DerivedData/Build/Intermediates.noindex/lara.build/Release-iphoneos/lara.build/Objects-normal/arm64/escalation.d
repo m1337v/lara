b35e9bbc3b3a4ba93c1b69581015ad7da424ca0a
roooot <103732419+rooootdev@users.noreply.github.com>
2026-03-25 14:59:57 +0100

    add stuff

--- build/DerivedData/Build/Intermediates.noindex/lara.build/Release-iphoneos/lara.build/Objects-normal/arm64/escalation.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/ruter/Documents/GitHub/lara/lara/kexploit/escalation.m \
-  /Users/ruter/Documents/GitHub/lara/lara/kexploit/escalation.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/17/include/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/include/DarwinFoundation2.modulemap \
-  /Users/ruter/Documents/GitHub/lara/lara/kexploit/darksword.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/include/DarwinFoundation3.modulemap
--- build/DerivedData/Build/Intermediates.noindex/lara.build/Release-iphoneos/lara.build/Objects-normal/arm64/kroot.d
+++
@@ -1,5 +0,0 @@
-dependencies: /Users/ruter/Documents/GitHub/lara/lara/kexploit/kroot.m \
-  /Users/ruter/Documents/GitHub/lara/lara/kexploit/kroot.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/17/include/module.modulemap \
-  /Users/ruter/Documents/GitHub/lara/lara/kexploit/darksword.h
